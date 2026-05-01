36a10902b88118dc1d7d10049d2c0d357e64603d
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-05-01 17:03:19 +0900

    delete unneed code

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -13,8 +13,6 @@ export const paths = {
   maintenance: '/maintenance',
   pricing: '/pricing',
   payment: '/payment',
-  about: '/about-us',
-  contact: '/contact-us',
   faqs: '/faqs',
   page403: '/error/403',
   page404: '/error/404',
