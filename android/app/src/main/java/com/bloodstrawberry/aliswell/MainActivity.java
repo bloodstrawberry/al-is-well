b967083c2c6a201cc004f2349ce8d69f5bd7da0a
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-05-02 18:19:18 +0900

    안드로이드 설정

---
+++ android/app/src/main/java/com/bloodstrawberry/aliswell/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bloodstrawberry.aliswell;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bloodstrawberry.aliswell',
+  appName: 'al-is-well',
+  webDir: 'out'
+};
+
+export default config;
