d3c08bd7fcbce0e3643a06677b826c091265bb0f
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-05-03 22:51:25 +0900

    Update motion-lazy.tsx

--- src/components/animate/motion-lazy.tsx
+++ src/components/animate/motion-lazy.tsx
@@ -1,14 +1,6 @@
 'use client';
 
-import { LazyMotion } from 'framer-motion';
-
-// ----------------------------------------------------------------------
-
-export type MotionLazyProps = {
-  children: React.ReactNode;
-};
-
-import { domMax } from 'framer-motion';
+import { LazyMotion, domMax } from 'framer-motion';
 
 // ----------------------------------------------------------------------
 
