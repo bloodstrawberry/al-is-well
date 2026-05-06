e081552f75351cc6c785db0332f9c2a2fccb82e4
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-05-06 23:41:35 +0900

    Revert "Update iconify.tsx"
    This reverts commit 5c6d4df0d0c362eacb23d1e2a3d82e7e4561a90b.

--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -1,26 +1,18 @@
 'use client';
 
-import type { BoxProps } from '@mui/material/Box';
-import type { Theme, SxProps } from '@mui/material/styles';
-
 import Box from '@mui/material/Box';
 
-export type IconifyProps = BoxProps & {
-  icon?: string;
-};
-
-export function Iconify({ width = 20, height, sx, icon, ...other }: IconifyProps) {
+export function Iconify({ width = 20, height, sx, ...other }: any) {
   return (
     <Box
       component="span"
-      className="iconify"
       sx={[
         {
           width,
           height: height ?? width,
           display: 'inline-flex',
         },
-        ...(Array.isArray(sx) ? sx : [sx as SxProps<Theme>]),
+        ...(Array.isArray(sx) ? sx : [sx]),
       ]}
       {...other}
     />
