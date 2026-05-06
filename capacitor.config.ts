ced565e8833181f6a97be8bde50a2ec9d64be0a3
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-05-07 01:27:56 +0900

    delete build error

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.bloodstrawberry.aliswell',
-  appName: 'al-is-well',
-  webDir: 'out'
-};
-
-export default config;
