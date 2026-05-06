5c6d4df0d0c362eacb23d1e2a3d82e7e4561a90b
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-05-06 22:28:37 +0900

    Update iconify.tsx

--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -1,18 +1,26 @@
 'use client';
 
+import type { BoxProps } from '@mui/material/Box';
+import type { Theme, SxProps } from '@mui/material/styles';
+
 import Box from '@mui/material/Box';
 
-export function Iconify({ width = 20, height, sx, ...other }: any) {
+export type IconifyProps = BoxProps & {
+  icon?: string;
+};
+
+export function Iconify({ width = 20, height, sx, icon, ...other }: IconifyProps) {
   return (
     <Box
       component="span"
+      className="iconify"
       sx={[
         {
           width,
           height: height ?? width,
           display: 'inline-flex',
         },
-        ...(Array.isArray(sx) ? sx : [sx]),
+        ...(Array.isArray(sx) ? sx : [sx as SxProps<Theme>]),
       ]}
       {...other}
     />
